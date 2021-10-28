dd7d011f7b2b1ed39ae0cc17c4b80eef83b8fdf5
Alfonso Rios <manuel.rios@ironbit.com.mx>
2021-10-28 18:48:20 -0500

    feature(RequestManager): Add a RequestManager for API request layer

---
+++ packages/requester-manager/RequestManager.js
@@ -0,0 +1,35 @@
+export class RequestManager {
+    constructor(host) {
+        this.host = host || '';
+    }
+
+    async request(config) {
+        const params = {};
+
+        const host = config.host || this.host;
+        const endpoint = config.endpoint;
+        const method = config.method || 'GET';
+        params.method = method;
+        let body = '{}';
+        let headers = {
+            'Content-Type': 'application/json'
+        };
+        if (config.headers && Object.keys(config.headers).length > 0) {
+            headers = {...headers, ...config.headers};
+        }
+        params.headers = headers;
+        if (method === 'POST' || method === 'PUT') {
+            body = JSON.stringify(config.body);
+            params.body = body;
+        }
+        const response = await fetch(`${host}/${endpoint}`, params);
+        if (config.callbacks && config.callbacks.length > 0) {
+            for (const callback of config.callbacks) {
+                if (response.status === callback.status) {
+                    callback.action();
+                }
+            }
+        }
+        return await response.json();
+    }
+}
